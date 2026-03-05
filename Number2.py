b765875fb14c793c47a5d19f94bce6edae35b263
vanditha-Kurmapu <vanditha.kurmpau@gmail.com>
2026-03-05 21:08:59 +0530

    Added

---
+++ Number2.py
@@ -0,0 +1,7 @@
+radius = 84  
+pi = 3.14
+water_per_sqm = 1.4  
+area = pi * radius ** 2
+total_water = area * water_per_sqm
+print("Area of the pond (m²):", int(area))       
+print("Total water in the pond (liters):", int(total_water))  
